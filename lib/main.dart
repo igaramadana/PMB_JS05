1509e6a8797100826da88aa6c670aa2ac363caa2
igaramadana <igrmdns085@gmail.com>
2025-10-01 09:32:19 +0700

    Praktikum 4 : Langkah 2 Menambahkan gambar ke body

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,18 @@ class MyApp extends StatelessWidget {
       title: 'Flutter layout: Iga Ramadana Sahputra - 2341760083',
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter layout demo')),
-        body: Column(children: [titleSection, buttonSection, textSection]),
+        body: Column(
+          children: [
+            Image.asset(
+              'images/image.png',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            buttonSection,
+            textSection,
+          ],
+        ),
       ),
     );
   }
